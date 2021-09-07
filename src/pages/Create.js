27df581e731361d8fba07205bd881fd7cd1ff162
Piotr Wnuczek <piotrantoniwnuczek@gmail.com>
2021-09-07 18:20:58 +0200

    update

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Container, Typography, Button } from '@material-ui/core';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
 const Create = () => (
-  <div>
-    Create
-  </div>
+  <Container>
+    <Typography
+      variant='h6'
+      component='h2'
+      color='textSecondary'
+      gutterBottom
+    >
+      Create a New Note
+    </Typography>
+    <Button
+      type='submit'
+      variant='contained'
+      color='secondary'
+      endIcon={<KeyboardArrowRightIcon />}
+    >
+      Submit
+    </Button>
+  </Container>
 );
 
 export default Create;
