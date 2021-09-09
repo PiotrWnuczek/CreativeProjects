ff4b6e96afd7d9553fa53f38f861a399170751ab
Piotr Wnuczek <piotrantoniwnuczek@gmail.com>
2021-09-09 11:26:14 +0200

    theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
+import React from 'react';
+import { ThemeProvider } from '@material-ui/core';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { createTheme } from '@material-ui/core/styles';
+import { purple } from '@material-ui/core/colors';
 import Notes from 'pages/Notes';
 import Create from 'pages/Create';
 
+const theme = createTheme({
+  palette: {
+    secondary: purple,
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  },
+});
+
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path='/'><Notes /></Route>
-      <Route path='/create'><Create /></Route>
-    </Switch>
-  </BrowserRouter>
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/'><Notes /></Route>
+        <Route path='/create'><Create /></Route>
+      </Switch>
+    </BrowserRouter>
+  </ThemeProvider>
 );
 
 export default App;
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,26 +1,36 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core';
 import { Container, Typography, Button } from '@material-ui/core';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
-const Create = () => (
-  <Container>
-    <Typography
-      variant='h6'
-      component='h2'
-      color='textSecondary'
-      gutterBottom
-    >
-      Create a New Note
-    </Typography>
-    <Button
-      type='submit'
-      variant='contained'
-      color='secondary'
-      endIcon={<KeyboardArrowRightIcon />}
-    >
-      Submit
-    </Button>
-  </Container>
-);
+const useStyles = makeStyles({
+  btn: { fontSize: 20},
+});
+
+const Create = () => {
+  const classes = useStyles();
+
+  return (
+    <Container>
+      <Typography
+        variant='h6'
+        component='h2'
+        color='textSecondary'
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+      <Button
+        className={classes.btn}
+        type='submit'
+        variant='contained'
+        color='secondary'
+        endIcon={<KeyboardArrowRightIcon />}
+      >
+        Submit
+      </Button>
+    </Container>
+  )
+};
 
 export default Create;
