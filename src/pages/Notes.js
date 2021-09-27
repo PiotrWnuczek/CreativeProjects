0e4cff287b7e2283e187b744943794a2db7b2438
Piotr Wnuczek <piotrantoniwnuczek@gmail.com>
2021-09-27 11:58:41 +0200

    update

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Container, Grid, Paper } from '@material-ui/core';
 
 const Notes = () => {
   const [notes, setNotes] = useState([]);
@@ -10,11 +11,15 @@ const Notes = () => {
   }, []);
 
   return (
-    <div>
-      {notes.map(note =>
-        <p key={note.id}>{note.title}</p>
-      )}
-    </div>
+    <Container>
+      <Grid container>
+        {notes.map(note =>
+          <Grid item xs={12} md={6} lg={4} key={note.id}>
+            <Paper>{note.title}</Paper>
+          </Grid>
+        )}
+      </Grid>
+    </Container>
   )
 };
 
