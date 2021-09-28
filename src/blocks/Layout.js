772f7f13798fc531f805ee178a06615a8638b074
Piotr Wnuczek <piotrantoniwnuczek@gmail.com>
2021-09-28 10:22:23 +0200

    drawer

--- src/blocks/Layout.js
+++ src/blocks/Layout.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, Drawer, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles({
   page: {
     background: '#f9f9f9',
     width: '100%',
+    height: '100%',
+  },
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    width: 250,
+  },
+  paper: {
+    width: 250,
   },
 });
 
@@ -12,8 +22,20 @@ const Layout = ({ children }) => {
   const classes = useStyles();
 
   return (
-    <div className={classes.page}>
-      {children}
+    <div className={classes.root}>
+      <Drawer
+        className={classes.drawer}
+        classes={{ paper: classes.paper }}
+        variant='permanent'
+        anchor='left'
+      >
+        <Typography variant='h5'>
+          Material App
+        </Typography>
+      </Drawer>
+      <div className={classes.page}>
+        {children}
+      </div>
     </div>
   )
 };
